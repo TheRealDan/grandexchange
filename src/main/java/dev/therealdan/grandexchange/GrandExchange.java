01618cefdc0037ed8b94051cd3af6e410ff055e7
TheRealDan <thedanielhailwood@gmail.com>
2020-11-23 13:07:35 +1300

    GrandExchange: setup vault economy in onEnable()

--- src/main/java/dev/therealdan/grandexchange/GrandExchange.java
+++ src/main/java/dev/therealdan/grandexchange/GrandExchange.java
@@ -1,17 +1,22 @@
 package dev.therealdan.grandexchange;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class GrandExchange extends JavaPlugin {
 
+    private Economy economy = null;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        setupEconomy();
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    private void setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) return;
+        RegisteredServiceProvider<Economy> serviceProvider = getServer().getServicesManager().getRegistration(Economy.class);
+        if (serviceProvider == null) return;
+        economy = serviceProvider.getProvider();
     }
 }
